a4edcf3217d1ea088d6cfdd439671d9e986c531a
Ruslan Akunevich <ruslan.ok@gmail.com>
2025-04-16 23:57:02 +0200

    Password Generator Dialog UI part

--- password_manager/static/src/views/buttons/generate_password_button/generate_password_button.js
+++ password_manager/static/src/views/buttons/generate_password_button/generate_password_button.js
@@ -1,4 +1,3 @@
-// import { _t } from "@web/core/l10n/translation";
 import { PasswordGeneratorDialog } from "../../dialogs/password_generator_dialog";
 import { useService } from "@web/core/utils/hooks";
 import { Component } from "@odoo/owl";
@@ -13,44 +12,13 @@ export class GeneratePasswordButton extends Component {
     setup() {
         super.setup();
         this.dialog = useService("dialog");
-        // this.useGeneratedValue = this._useGeneratedValue.bind(this);
     }
 
     async onClick() {
         this.dialog.add(PasswordGeneratorDialog, {
+            title: "Generator",
             value: this.props.value,
             useGeneratedValue: this.props.useGeneratedValue,
         });
     }
-
-    // useGeneratedValue(
-    //     value,
-    //     isPassphrase, 
-    //     len, 
-    //     useCapitalLetters, 
-    //     useLowercaseLetters, 
-    //     useDigits, 
-    //     useSpecials, 
-    //     minimumNumbers, 
-    //     minimumSpecials, 
-    //     avoidAmbiguous, 
-    //     wordSeparator
-    // ) {
-    //     this.props.useGeneratedValue(
-    //         value,
-    //         isPassphrase, 
-    //         len, 
-    //         useCapitalLetters, 
-    //         useLowercaseLetters, 
-    //         useDigits, 
-    //         useSpecials, 
-    //         minimumNumbers, 
-    //         minimumSpecials, 
-    //         avoidAmbiguous, 
-    //         wordSeparator
-    //     );
-    //     console.log("value: " + value);
-    // }
-
-
 }
--- password_manager/static/src/views/dialogs/password_generator_dialog.js
+++ password_manager/static/src/views/dialogs/password_generator_dialog.js
@@ -70,12 +70,21 @@ export class PasswordGeneratorDialog extends Component {
     setup() {
         this.successText = _t("Copied");
         this.value_input = useRef("value_input");
-        this.state = useState({value: this.props.value});
+        const isPassphrase = this.getStorageItem("is_passphrase") === "1";
+        this.passwordLength = this.getStorageItem("password_length") || 15;
+        this.numberOfWords = this.getStorageItem("number_of_words") || 6;
+        this.state = useState({
+            value: this.props.value,
+            isPassphrase: isPassphrase,
+            passwordLength: isPassphrase ? this.numberOfWords : this.passwordLength,
+            minimumNumbers: this.getStorageItem("minimum_numbers") || 1,
+            minimumSpecials: this.getStorageItem("minimum_specials") || 1,
+            wordSeparator: this.getStorageItem("word_separator") || "-",
+            capitalize: this.getStorageItem("capitalize") === "1",
+            includeNumber: this.getStorageItem("include_number") === "1",
+        });
         this.passphraseChoices = PASSPHRASE_CHOICES;
         this.env.dialogData.dismiss = () => this.discardRecord();
-        this.params = {
-            isPassphrase: this.getStorageItem("is_passphrase"),
-        };
         let includeValues = [];
         INCLUDE_VALUES.forEach((vals) => {
             let newVals = vals;
@@ -86,14 +95,8 @@ export class PasswordGeneratorDialog extends Component {
             includeValues.push(newVals);
         });
         this.includeValues = includeValues;
+        this.debugValue = 100;
         this.generateNewValue();
-    }
-
-    useThisValue() {
-        this.props.useGeneratedValue(
-            this.state.value,
-        );
-        this.props.close();
     }
 
     discardRecord() {
@@ -101,7 +104,9 @@ export class PasswordGeneratorDialog extends Component {
     }
 
     generateNewValue() {
-        console.log("generateNewValue");
+        this.debugValue++;
+        this.state.value = this.debugValue.toString();
+        console.log("generateNewValue: " + this.state.value);
     }
 
     updateValue() {
@@ -114,10 +119,90 @@ export class PasswordGeneratorDialog extends Component {
 
     setStorageItem(name, value) {
         localStorage.setItem(name, value);
+    }
+
+    togglePasswordPassphrase(value) {
+        console.log("togglePasswordPassphrase: " + value);
+        this.setStorageItem("is_passphrase", value);
+        this.state.isPassphrase = value === "1";
+        this.state.passwordLength = this.state.isPassphrase ? this.numberOfWords : this.passwordLength;
         this.generateNewValue();
     }
 
     updateInclude(data) {
         data.forEach(item => this.setStorageItem("include_" + item.name, item.checked ? "1" : "0"));
+        this.generateNewValue();
+    }
+
+    get passwordLengthLabel() {
+        if (this.state.isPassphrase)
+            return _t("Number of words");
+        return _t("Length");
+    }
+
+    get passwordLengthClarification() {
+        if (this.state.isPassphrase)
+            return _t("Value must be between 3 and 20. Use 6 words or more to generate a strong passphrase.");
+        return _t("Value must be between 10 and 128. Use 14 characters or more to generate a strong password.");
+    }
+
+    get minLength() {
+        if (this.state.isPassphrase)
+            return "3";
+        return "10";
+    }
+
+    get maxLength() {
+        if (this.state.isPassphrase)
+            return "20";
+        return "128";
+    }
+
+    get isMinimumNumbersDisabled() {
+        return !this.includeValues.filter(x => x.name === "numbers")[0].checked;
+    }
+
+    get isMinimumSpecialsDisabled() {
+        return !this.includeValues.filter(x => x.name === "special")[0].checked;
+    }
+
+    lengthChanged(el) {
+        console.log("lengthChanged: " + el.target.value);
+        if (this.state.isPassphrase) {
+            this.numberOfWords = el.target.value;
+            this.setStorageItem("number_of_words", this.numberOfWords);
+            this.state.passwordLength = this.numberOfWords;
+        } else {
+            this.passwordLength = el.target.value;
+            this.setStorageItem("password_length", this.passwordLength);
+            this.state.passwordLength = this.passwordLength;
+        }
+        this.generateNewValue();
+    }
+
+    wordSeparatorChanged(el) {
+        console.log("wordSeparatorChanged: " + el.target.value);
+        this.state.wordSeparator = el.target.value;
+        this.setStorageItem("word_separator", this.state.wordSeparator);
+        this.generateNewValue();
+    }
+
+    capitalizeChanged(el) {
+        console.log("capitalizeChanged: " + el.target.checked);
+    }
+
+    includeNumberChanged(el) {
+        console.log("includeNumberChanged: " + el.target.checked);
+    }
+
+    useThisValue() {
+        this.props.useGeneratedValue(
+            this.state.value,
+        );
+        this.props.close();
+    }
+
+    showHistory() {
+        console.log("showHistory");
     }
 }
