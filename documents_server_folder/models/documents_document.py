b32d3afa001591b457a652f50e05e7c26ed066cb
Ruslan Akunevich <ruslan.ok@gmail.com>
2025-07-08 04:07:34 +0000

    Fixed search_panel_select_range method

--- documents_server_folder/models/documents_document.py
+++ documents_server_folder/models/documents_document.py
@@ -3,6 +3,8 @@ import mimetypes
 from collections import OrderedDict
 from odoo import models, api, _, fields
 from odoo.osv import expression
+from odoo.addons.documents.models.documents_document import Document as Document_for_patching
+
 
 class Document(models.Model):
     _inherit = "documents.document"
@@ -11,32 +13,26 @@ class Document(models.Model):
     has_children = fields.Boolean("Does it have child folders?", default=False)
     fetch_dt = fields.Datetime()
 
+    def _register_hook(self):
+        super()._register_hook()
+
+        @api.model
+        def search_panel_select_range_patch(self, field_name, **kwargs):
+            return self.search_panel_select_range_patched(field_name, **kwargs)
+
+        Document_for_patching.search_panel_select_range = search_panel_select_range_patch
+
     @api.model
-    def search_panel_select_range(self, field_name, **kwargs):
-        result = super().search_panel_select_range(field_name)
-
-        if field_name == "folder_id" and not self.env.user.share:
-            root_folder = self.env["documents.document"].search([
-                ("type", "=", "folder"),
-                ("folder_id", "=", False),
-                ("located_on_the_server", "=", True),
-                ("name", "=", "SERVER_FOLDER"),
-                ("owner_id", "=", self.env.user.id),
-                ])
-            if not root_folder:
-                root_folder = self.create_folder(False, "SERVER_FOLDER")
-                root_folder.populate_folder("")
-
+    def search_panel_select_range_patched(self, field_name, **kwargs):
+        if field_name == "folder_id":
             enable_counters = kwargs.get("enable_counters", False)
             search_panel_fields = ["access_token", "company_id", "description", "display_name", "folder_id",
-                "is_favorited", "is_pinned_folder", "owner_id", "shortcut_document_id",
-                "user_permission", "active", "alias_name", "alias_domain_id", "alias_tag_ids", "partner_id",
-                "create_activity_type_id", "create_activity_user_id"]
-            domain = [
-                ("type", "=", "folder"),
-                ("located_on_the_server", "=", True),
-                ("id", "!=", root_folder.id),
-            ]
+                                    "is_favorited", "is_pinned_folder", "owner_id", "shortcut_document_id",
+                                    "user_permission", "active"]
+            if not self.env.user.share:
+                search_panel_fields += ["alias_name", "alias_domain_id", "alias_tag_ids", "partner_id",
+                                        "create_activity_type_id", "create_activity_user_id", "located_on_the_server"]
+            domain = [("type", "=", "folder")]
 
             if unique_folder_id := self.env.context.get("documents_unique_folder_id"):
                 values = self.env["documents.document"].search_read(
@@ -75,37 +71,97 @@ class Document(models.Model):
                 ])
                 domain_image = self._search_panel_domain_image(field_name, model_domain, enable_counters)
 
+            # Read the targets in batch
+            targets = self.browse(r["shortcut_document_id"][0] for r in records if r["shortcut_document_id"])
+            targets_user_permission = {t.id: t.user_permission for t in targets}
+
             values_range = OrderedDict()
+            shared_root_id = "SHARED" if not self.env.user.share else False
+            server_folder = self.env["documents.document"].search(
+                [
+                    ("located_on_the_server", "=", True),
+                    ("folder_id", "=", False),
+                    ("name", "=", "SERVER_FOLDER"),
+                ]
+            )
             for record in records:
                 record_id = record["id"]
-                record["alias_tag_ids"] = [alias_tag_data[tag_id] for tag_id in record["alias_tag_ids"]]
+                if not self.env.user.share:
+                    record["alias_tag_ids"] = [alias_tag_data[tag_id] for tag_id in record["alias_tag_ids"]]
                 if enable_counters:
                     image_element = domain_image.get(record_id)
                     record["__count"] = image_element["__count"] if image_element else 0
+                if record["shortcut_document_id"]:
+                    record["target_user_permission"] = targets_user_permission[record["shortcut_document_id"][0]]
                 folder_id = record["folder_id"]
-                record["folder_id"] = "SERVER_FOLDER" if folder_id[0] == root_folder.id else folder_id[0]
+                if folder_id:
+                    folder_id = folder_id[0]
+                    if folder_id not in accessible_folder_ids:
+                        if record["shortcut_document_id"]:
+                            continue
+                        folder_id = shared_root_id
+                    if folder_id == server_folder.id:
+                        folder_id = "SERVER_FOLDER"
+                elif record["owner_id"][0] == self.env.user.id:
+                    if record["located_on_the_server"]:
+                        continue
+                    else:
+                        folder_id = "MY"
+                elif record["owner_id"][0] != self.env.ref("base.user_root").id or self.env.user.share:
+                    if record["shortcut_document_id"]:
+                        continue
+                    folder_id = shared_root_id
+                else:
+                    folder_id = "COMPANY"
+
+                record["folder_id"] = folder_id
                 values_range[record_id] = record
 
             if enable_counters:
                 self._search_panel_global_counters(values_range, "folder_id")
 
-            result["values"] += list(values_range.values())
+            special_roots = []
+            if not self.env.user.share:
+                special_roots = [
+                    {"bold": True, "childrenIds": [], "parentId": False, "user_permission": "edit"} | values
+                    for values in [
+                        {
+                            "display_name": _("Company"),
+                            "id": "COMPANY",
+                            "description": _("Common roots for all company users."),
+                            "user_permission": "view",
+                        }, {
+                            "display_name": _("My Drive"),
+                            "id": "MY",
+                            "user_permission": "edit",
+                            "description": _("Your individual space."),
+                        }, {
+                            "display_name": _("Server Folder"),
+                            "id": "SERVER_FOLDER",
+                            "user_permission": "edit",
+                            "description": _("Your individual space."),
+                        }, {
+                            "display_name": _("Shared with me"),
+                            "id": "SHARED",
+                            "description": _("Additional documents you have access to."),
+                        }, {
+                            "display_name": _("Recent"),
+                            "id": "RECENT",
+                            "description": _("Recently accessed documents."),
+                        }, {
+                            "display_name": _("Trash"),
+                            "id": "TRASH",
+                            "description": _("Items in trash will be deleted forever after %s days.",
+                                self.get_deletion_delay()),
+                        }]
+                ]
 
-            my_server_folder = {
-                "bold": True,
-                "childrenIds": [],
-                "parentId": False,
-                "user_permission": "edit",
-                "display_name": _("Server Folder"),
-                "id": "SERVER_FOLDER",
-                "user_permission": "edit",
-                "description": _("Your individual space on the Server."),
+            return {
+                "parent_field": "folder_id",
+                "values": list(values_range.values()) + special_roots,
             }
-            for i in range(len(result["values"]) - 1, -1, -1):
-                if result["values"][i].get("id") == "MY":
-                    result["values"].insert(i + 1, my_server_folder)
-                    break
-        return result
+
+        return super().search_panel_select_range(field_name)
 
     @property
     @api.model
