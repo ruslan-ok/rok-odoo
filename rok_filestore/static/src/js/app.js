895c6ee64d8ab8381f390e88aaa1ad73d9b400e6
Ruslan Akunevich <ruslan.ok@gmail.com>
2025-07-12 21:58:33 +0000

    rok_filestore module deleted

--- rok_filestore/static/src/js/app.js
+++
@@ -1,6 +0,0 @@
-/** @odoo-module */
-
-import { FilestoreApp } from "./components/FilestoreApp";
-import { registry } from "@web/core/registry";
-
-registry.category("actions").add("rok_filestore.filestore_app", FilestoreApp);
