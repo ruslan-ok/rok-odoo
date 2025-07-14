036890198564731a56e65de3a19eb857633c508d
Ruslan Akunevich <ruslan.ok@gmail.com>
2025-07-14 02:56:43 +0000

    Contact Birthday

---
+++ rok_google_contacts/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Birthday in Contacts",
+    "version": "18.0.1.0.0",
+    "depends": [
+        "odoo_google_contact_integration",
+    ],
+    "category": "Productivity/Reminder",
+    "author": "Ruslan Akunevich",
+    "website": "https://github.com/ruslan-ok/rok-odoo",
+    "summary": "Contact's Birthday and its synchronization with Google Contacts.",
+    "description": "Contact's Birthday and its synchronization with Google Contacts.",
+    "data": [
+        "views/res_partner_views.xml",
+    ],
+    "sequence": 49,
+    "application": False,
+    "license": "LGPL-3",
+    "assets": {},
+}
