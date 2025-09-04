e153599840fbd41833e138c504a015ce9426a5bc
ruslan-ok <ruslan.ok@gmail.com>
2025-09-04 19:06:10 +0000

    [ADD] rok_health

---
+++ rok_health/models/anthropometry.py
@@ -0,0 +1,29 @@
+from odoo import models, fields, api
+
+class Anthropometry(models.Model):
+    _name = 'rok.health.anthropometry'
+    _description = 'Anthropometry'
+
+    # The owner of the measurement; default to the current user
+    user_id = fields.Many2one(
+        'res.users',
+        string='User',
+        required=True,
+        index=True,
+        default=lambda self: self.env.user,
+    )
+
+    # Timestamp when the measurement was taken
+    measurement = fields.Datetime(
+        string='Date and Time of Measurement',
+        required=True,
+        default=fields.Datetime.now,
+    )
+
+    height = fields.Integer()
+    weight = fields.Float()
+    waist = fields.Float()
+    temperature = fields.Float()
+    systolic = fields.Float()
+    diastolic = fields.Float()
+    pulse = fields.Integer()
