12443544b8312ae5f938d0694cfa8eb240e6ad0f
ruslan-ok <ruslan.ok@gmail.com>
2025-09-11 20:28:36 +0000

    [IMP] rok_history

--- rok_history/models/rok_history_category.py
+++ rok_history/models/rok_history_category.py
@@ -1,68 +1,10 @@
-from odoo import api, fields, models, _
-from odoo.exceptions import ValidationError
+from odoo import models, fields
 
 class HistoryCategory(models.Model):
     _name = 'rok.history.category'
-    _inherit = ["mail.thread"]
     _description = 'History Category'
-    _parent_name = "parent_id"
-    _parent_store = True
-    _rec_name = "complete_name"
-    _order = "complete_name"
+    _order = 'name'
 
-    name = fields.Char("Name", index="trigram", required=True)
-    complete_name = fields.Char(
-        "Complete Name", compute="_compute_complete_name", recursive=True,
-        store=True)
-    parent_id = fields.Many2one("rok.history.category", "Parent Category", index=True, ondelete="cascade")
-    parent_path = fields.Char(index=True)
-    child_id = fields.One2many("rok.history.category", "parent_id", "Child Categories")
-    categ_id = fields.Many2one(
-        "rok.history.category", "History Category",
-        change_default=True,
-        group_expand="_read_group_categ_id",
-        required=True,
-    )
-    event_count = fields.Integer(
-        "# Events", compute="_compute_event_count",
-        help="The number of events under this category (Does not consider the children categories)")
+    name = fields.Char(string='Name', required=True)
+    description = fields.Text(string='Description')
     active = fields.Boolean(default=True)
-
-    def _compute_complete_name(self):
-        for category in self:
-            if category.parent_id:
-                category.complete_name = "%s / %s" % (category.parent_id.complete_name, category.name)
-            else:
-                category.complete_name = category.name
-
-    def _compute_event_count(self):
-        read_group_res = self.env["rok.history.event"]._read_group([("categ_id", "child_of", self.ids)], ["categ_id"], ["__count"])
-        group_data = {category.id: count for category, count in read_group_res}
-        for category in self:
-            event_count = 0
-            for sub_category_id in category.search([("id", "child_of", category.ids)]).ids:
-                event_count += group_data.get(sub_category_id, 0)
-            category.event_count = event_count
-
-    @api.constrains("parent_id")
-    def _check_category_recursion(self):
-        if self._has_cycle():
-            raise ValidationError(_("You cannot create recursive categories."))
-
-    @api.model
-    def name_create(self, name):
-        category = self.create({"name": name})
-        return category.id, category.display_name
-
-    @api.depends_context("hierarchical_naming")
-    def _compute_display_name(self):
-        if self.env.context.get("hierarchical_naming", True):
-            return super()._compute_display_name()
-        for record in self:
-            record.display_name = record.name
-
-    def _read_group_categ_id(self, categories, domain):
-        category_ids = self.env.context.get("default_categ_id")
-        if not category_ids and self.env.context.get("group_expand"):
-            category_ids = categories.sudo()._search([], order=categories._order)
-        return categories.browse(category_ids)
--- rok_history/models/rok_history_event.py
+++ rok_history/models/rok_history_event.py
@@ -3,7 +3,8 @@ from odoo import models, fields, api
 class HistoryEvent(models.Model):
     _name = 'rok.history.event'
     _description = 'History Event'
-    _order = 'start_date desc'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+    _order = 'is_favorite desc, start_date desc'
 
     # The owner of the measurement; default to the current user
     user_id = fields.Many2one(
@@ -14,19 +15,30 @@ class HistoryEvent(models.Model):
         default=lambda self: self.env.user,
     )
     active = fields.Boolean(default=True)
+    is_favorite = fields.Boolean(string="Favorite")
     start_date = fields.Date(compute='_compute_start_stop_dates', store=True)
     stop_date = fields.Date(compute='_compute_start_stop_dates', store=True)
-    name = fields.Char()
+    title = fields.Char()
     info = fields.Html()
     is_disease = fields.Boolean(default=False)
     diagnosis = fields.Char()
     fact_ids = fields.One2many('rok.history.fact', 'event_id')
+    categ_id = fields.Many2one('rok.history.category', string='Category')
 
-    @api.depends('start_date', 'stop_date')
+    @api.depends('fact_ids.fact_date_time')
     def _compute_start_stop_dates(self):
         for record in self:
-            record.start_date = min(record.fact_ids.mapped('fact_date_time')).date() if record.fact_ids.fact_date_time else False
-            record.stop_date = max(record.fact_ids.mapped('fact_date_time')).date() if record.fact_ids.fact_date_time else False
+            if record.fact_ids:
+                dates = record.fact_ids.mapped('fact_date_time')
+                if dates:
+                    record.start_date = min(dates).date()
+                    record.stop_date = max(dates).date()
+                else:
+                    record.start_date = False
+                    record.stop_date = False
+            else:
+                record.start_date = False
+                record.stop_date = False
 
     def init_by_articles(self):
         pass
