9e911eb50fcb5414ca7e7db3f3abd6b833bf1841
ruslan-ok <ruslan.ok@gmail.com>
2025-09-26 10:25:53 +0000

    WARNING rok_odoo_empty odoo.registry: Model attribute '_sql_constraints' is no longer supported, please define model.Constraint on the model.

--- password_manager/models/password_tag.py
+++ password_manager/models/password_tag.py
@@ -1,5 +1,6 @@
 from random import randint
 from odoo import fields, models
+from odoo.models import Constraint
 
 class PasswordTag(models.Model):
     _name = "password.tag"
@@ -8,7 +9,6 @@ class PasswordTag(models.Model):
 
     def _get_default_password_id(self):
         return self.env["passwords"].browse(self.env.context.get("password_id"))
-
 
     def _get_default_color(self):
         return randint(1, 11)
@@ -24,13 +24,15 @@ class PasswordTag(models.Model):
     )
     active = fields.Boolean(default=True)
 
-    _sql_constraints = [
-        ("name_uniq", "unique (name)", "Tag name already exists!"),
-    ]
-
     def copy_data(self, default=None):
         vals_list = super().copy_data(default=default)
         return [dict(vals, name=self.env._("%s (copy)", tag.name)) for tag, vals in zip(self, vals_list)]
 
     def _search_password_ids(self, operator, operand):
         return [("password_ids.product_variant_ids", operator, operand)]
+
+    class NameUniqueConstraint(Constraint):
+        _name = "name_unique"
+        _model = "password.tag"
+        _sql = "UNIQUE(name)"
+        _message = "Tag name already exists!"
