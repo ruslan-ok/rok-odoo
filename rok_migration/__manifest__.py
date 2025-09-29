fe88b147663201a1f9223723113151b771e6f2be
ruslan-ok <ruslan.ok@gmail.com>
2025-09-29 06:40:24 +0000

    [ADD] rok_migration

---
+++ rok_migration/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Migrator',
+    'version': '1.0',
+    'depends': ['base'],
+    'category': 'Tools',
+    'description': 'Migrator',
+    'data': [
+    ],
+    'installable': True,
+    'author': 'Ruslan Akunevich',
+    "license": "LGPL-3",
+}
