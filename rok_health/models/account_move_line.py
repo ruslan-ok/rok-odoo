c65640be48e1de3a366a3c389d0d57c08be0c5ba
ruslan-ok <ruslan.ok@gmail.com>
2025-10-23 18:33:59 +0000

    Price saving

--- rok_health/models/account_move_line.py
+++ rok_health/models/account_move_line.py
@@ -6,3 +6,11 @@ class AccountMoveLine(models.Model):
     weight_g = fields.Integer(string='Weight, grams')
     pack_pcs = fields.Float(string='Pack Volume, pieces')
     kcal_100g = fields.Integer(string='Calories in 100 grams')
+
+    @api.onchange('product_id')
+    def _onchange_product_id(self):
+        if self.product_id and self.move_id.move_type == 'in_invoice' and self.product_id.categ_id == self.env.ref('rok_health.cat_food'):
+            self.weight_g = self.product_id.weight
+            self.pack_pcs = self.product_id.volume
+            self.kcal_100g = self.product_id.kcal_100g
+            self.price_unit = self.product_id.standard_price
