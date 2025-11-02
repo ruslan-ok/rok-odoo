be4cdbc7fc046368635c8fbd8075700cdb6c5b85
ruslan-ok <ruslan.ok@gmail.com>
2025-11-02 08:51:22 +0000

    [IMP] Age

--- rok_google_contacts/models/res_partner.py
+++ rok_google_contacts/models/res_partner.py
@@ -5,3 +5,17 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     birthday = fields.Date()
+    age = fields.Integer(compute='_compute_age')
+
+    def _compute_age(self):
+        today = fields.Date.today()
+        for record in self:
+            if record.birthday:
+                # Calculate age based on year difference
+                age = today.year - record.birthday.year
+                # Check if birthday hasn't occurred yet this year
+                if today.month < record.birthday.month or (today.month == record.birthday.month and today.day < record.birthday.day):
+                    age -= 1
+                record.age = age
+            else:
+                record.age = 0
