d73262d2584d4bcbecb708f4ed682d4268882e2f
ruslan-ok <ruslan.ok@gmail.com>
2025-12-20 13:16:21 +0000

    rok apps initial

---
+++ rok_apps_dashboard/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Rok Apps Dashboard",
+    "version": "1.0",
+    "summary": "Rok Apps Dashboard module",
+    "author": "Ruslan Akunevich",
+    "license": "LGPL-3",
+    "website": "https://ventor.tech",
+    "application": True,
+    "category": "Productivity/Rok Apps",
+    "depends": ["base"],
+    "data": [
+        "views/rok_apps_views.xml",
+    ],
+    "assets": {},
+}
